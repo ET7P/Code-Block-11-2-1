71f1c438bcc2201bffe0e880f3d267693d71e211
nnm314 <>
2022-12-12 16:51:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+	 string myName456;
+	 myName456 = "Jane";
+
+	 Console.WriteLine(myName456);
+	 
+	 Console.ReadKey();
+  }
+}
